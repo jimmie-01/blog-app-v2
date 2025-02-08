281e8911bdbf9d11e978a67b9eafbe1f69b6ea54
Fakile Razaq <razaq_jimmie@yahoo.com>
2025-02-08 13:03:02 +0100

    Render Post to home from DB

--- controllers/blogControllers.js
+++ controllers/blogControllers.js
@@ -1,6 +1,25 @@
+const Post = require('../server/models/post');
 
-module.exports.get_blog = (req, res) => {
-	res.render('index');
+// ROUTES
+
+/**
+ * GET /
+ * Home Page
+ */
+module.exports.get_blog = async (req, res) => {
+
+	try {
+		const locals = {
+			title: "Nodejs Blog-v2",
+			Description: "Simple Blog Created with Nodejs, Express and MongoDB"
+		}
+	
+		const data = await Post.find().sort({ createdAt: -1});
+	
+		res.render('index', { locals, data });
+	} catch (error) {
+		console.log(error);
+	}
 };
 
 module.exports.get_about = (req, res) => {
