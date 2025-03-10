1b2a57f60809ee6a09bcebad61e374696953b1a3
Fakile Razaq <razaq_jimmie@yahoo.com>
2025-03-10 12:44:45 +0100

    Debbuging in Progress

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -45,22 +45,6 @@ module.exports.post_login = async(req, res) => {
  */
 
 module.exports.post_register = async (req, res) => {
-	try {
-		
-		const { username, password } = req.body;
-		const hashedPassword = await bycrypt.hash(password, 10);
-
-		try {
-			const user = await User.create({ username, password: hashedPassword });
-			res.status(201).json({ message: 'User created', user });
-		} catch (error) {
-			if(error.code === 11000) {
-				res.status(409).json({ message: 'User Already in use' });
-			}
-			res.status(500).json({ message: 'Internal Server Error'});
-		}
-
-	} catch (error) {
-		console.log(error);
-	}
+	const { username, password} = req.body;
+	res.status(201).send({ username, password });
 }
--- server/models/User.js
+++ server/models/User.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const UserSchema = new Schema({
 	username: {
 		type: String,
-		require: true,
+		required: true,
 		unique: true
 	},
 	password: {
